5b4f6c3b5955be844a746523ae4d8d685e0a50f9
criskell <cris.moraes.ds@gmail.com>
2025-08-27 22:19:25 -0300

    feat: store tasks with in-memory repository

--- src/main/java/com/criskell/epictask/controller/TaskController.java
+++ src/main/java/com/criskell/epictask/controller/TaskController.java
@@ -1,29 +1,28 @@
 package com.criskell.epictask.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.criskell.epictask.model.Task;
 
 @Controller
 @RequestMapping("/tasks")
 class TaskController {
+    private List<Task> tasks = new ArrayList<>(List.of(
+        new Task(1L, "Aprender ELF", "Aprender o formato de executáveis do Linux", 0, 0),
+        new Task(2L, "Aprender Monad Transformers", "Aprender como compor entre diferentes monads", 0, 0),
+        new Task(3L, "Aprender árvores B+Tree",
+                "Aprender como funciona as árvores utilizadas por índices em bancos de dados", 0, 0),
+        new Task(4L, "Aprender árvore AVL", "Aprender como funciona a árvore AVL", 0, 0)));
+
     @GetMapping
     public String list(Model model) {
-        var tasks = List.of(
-                new Task(1L, "Aprender ELF", "Aprender o formato de executáveis do Linux", 0, 0),
-                new Task(2L, "Aprender Monad Transformers", "Aprender como compor entre diferentes monads", 0, 0),
-                new Task(3L, "Aprender árvores B+Tree",
-                        "Aprender como funciona as árvores utilizadas por índices em bancos de dados", 0, 0),
-                new Task(4L, "Aprender árvore AVL", "Aprender como funciona a árvore AVL", 0, 0));
-
         model.addAttribute("tasks", tasks);
 
         return "tasks/index";
@@ -35,9 +34,8 @@ class TaskController {
     }
 
     @PostMapping("/form")
-    @ResponseBody
     public String create(Task task) {
-        System.out.println("cadastrando tarefa... " + task);
-        return "form";
+        tasks.add(task);
+        return "redirect:/tasks";
     }
 }
