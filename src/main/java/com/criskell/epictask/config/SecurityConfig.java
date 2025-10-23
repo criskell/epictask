df3a15c64f043f688f90ed4a1a782c7b6114c37b
criskell <cris.moraes.ds@gmail.com>
2025-10-22 22:24:03 -0300

    feat: add oauth2 authentication with github and google

---
+++ src/main/java/com/criskell/epictask/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.criskell.epictask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                    .authorizeHttpRequests((auth) -> auth.requestMatchers("/tasks/**").authenticated()
+                        .requestMatchers("/").permitAll()    
+                    )
+                    .oauth2Login(Customizer.withDefaults())
+                    .build();
+    }
+}
